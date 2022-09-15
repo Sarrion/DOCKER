a48e7d9457c62ae1bf99690c24a4422ff1850ed8
sarrion <sarrionmas.a@gmail.com>
2022-09-15 12:39:24 +0200

    cpp deb for vimspector

---
+++ sarrion_vimspector/fib.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i; 
+    int numTerms = 10;
+    int t1 = 0, t2 = 1;
+    int nextTerm;
+	
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= numTerms; ++i) 
+    {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
